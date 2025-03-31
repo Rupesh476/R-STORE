223786cd13d8bd467170e3277412b2619aba2724
Rupesh476 <sahrupesh288@gmail.com>
2025-03-31 13:16:04 +0545

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 
 const Add = () => {
 
-    const url = "http://localhost:4000";
+    const url = "https://r-store-frontend.onrender.com";
 
     const[image,setImage] = useState(false);
     const[data,setData] = useState({
