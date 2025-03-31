2eb71afdcaa6d13b446659c24d00107d74511740
Rupesh476 <sahrupesh288@gmail.com>
2025-03-31 13:10:07 +0545

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // Ensure the key is loaded
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://r-store-frontend.onrender.com";
 
     try {
         const newOrder = new orderModel({
